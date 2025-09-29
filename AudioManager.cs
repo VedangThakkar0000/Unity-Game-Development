8e992325023ad8b2fe6c1146eadb10b8c0299505
VedangThakkar0000 <22csd17@svitvasad.ac.in>
2025-09-30 01:02:27 +0530

    Implement AudioManager for sound management

---
+++ AudioManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    [Header("Clips")]
+    [SerializeField] private AudioClip backgroundMusic;
+    [SerializeField] private AudioClip buttonClick;
+    [SerializeField] private AudioClip reelSpin;
+    [SerializeField] private AudioClip winSound;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        if (musicSource != null && backgroundMusic != null)
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
+    public void PlayButton()
+    {
+        PlaySFX(buttonClick);
+    }
+
+    public void PlaySpin()
+    {
+        PlaySFX(reelSpin);
+    }
+    public void PlayWin()
+    {
+        PlaySFX(winSound);
+    }
+    private void PlaySFX(AudioClip clip)
+    {
+        if (sfxSource != null && clip != null)
+            sfxSource.PlayOneShot(clip);
+    }
+}
